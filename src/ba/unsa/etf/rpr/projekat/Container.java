aeccbb1e0f89c20fd49a77c828fcc188a7f1c947
Faris Demir <fdemir1@etf.unsa.ba>
2020-02-01 23:50:36 +0100

    Kreirani atributi i generisani getteri i setteri za Container klasu

--- src/ba/unsa/etf/rpr/projekat/Container.java
+++ src/ba/unsa/etf/rpr/projekat/Container.java
@@ -1,4 +1,24 @@
 package ba.unsa.etf.rpr.projekat;
 
+import java.util.ArrayList;
+
 public class Container {
+    private String tag;
+    private ArrayList<Product> products;
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
 }
