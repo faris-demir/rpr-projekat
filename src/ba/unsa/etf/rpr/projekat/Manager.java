264228a9c4e6193c06ca6967bd24aacd556d8bb8
Faris Demir <fdemir1@etf.unsa.ba>
2020-02-02 01:38:00 +0100

    Kreirani atributi i generisani getteri i setteri za Manager klasu

--- src/ba/unsa/etf/rpr/projekat/Manager.java
+++ src/ba/unsa/etf/rpr/projekat/Manager.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr.projekat;
 
 public class Manager {
+    private String username = "admin";
+    private String password = "administrator";
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
