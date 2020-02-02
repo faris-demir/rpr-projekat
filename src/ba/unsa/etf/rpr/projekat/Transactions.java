04fe62f940acd1be21cc7862144e840c98638f6d
Faris Demir <fdemir1@etf.unsa.ba>
2020-02-02 01:39:12 +0100

    Kreirani atributi, getteri i setteri za Transactions klasu

--- src/ba/unsa/etf/rpr/projekat/Transactions.java
+++ src/ba/unsa/etf/rpr/projekat/Transactions.java
@@ -1,4 +1,24 @@
 package ba.unsa.etf.rpr.projekat;
 
+import java.util.ArrayList;
+
 public class Transactions {
+    private ArrayList<Sale> sales;
+    private ArrayList<Order> orders;
+
+    public ArrayList<Sale> getSales() {
+        return sales;
+    }
+
+    public void setSales(ArrayList<Sale> sales) {
+        this.sales = sales;
+    }
+
+    public ArrayList<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(ArrayList<Order> orders) {
+        this.orders = orders;
+    }
 }
