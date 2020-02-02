f1a02189a691fb0eb99d3d99c982e4fc8ce74a8e
Faris Demir <fdemir1@etf.unsa.ba>
2020-02-02 01:24:41 +0100

    Kreirani atributi i generisani getteri i setteri za Sale klasu

--- src/ba/unsa/etf/rpr/projekat/Sale.java
+++ src/ba/unsa/etf/rpr/projekat/Sale.java
@@ -1,4 +1,51 @@
 package ba.unsa.etf.rpr.projekat;
 
+import java.time.LocalDateTime;
+
 public class Sale {
+    private String productName;
+    private int soldQuantity;
+    private LocalDateTime saleDate;
+    private int price;
+    private int totalPrice;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getSoldQuantity() {
+        return soldQuantity;
+    }
+
+    public void setSoldQuantity(int soldQuantity) {
+        this.soldQuantity = soldQuantity;
+    }
+
+    public LocalDateTime getSaleDate() {
+        return saleDate;
+    }
+
+    public void setSaleDate(LocalDateTime saleDate) {
+        this.saleDate = saleDate;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
