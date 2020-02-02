5daf5a0c401998315df98118b5c43201fa2e0944
Faris Demir <fdemir1@etf.unsa.ba>
2020-02-03 00:23:07 +0100

    Dodana metoda za obnavljanje baze

--- src/ba/unsa/etf/rpr/projekat/WarehouseModel.java
+++ src/ba/unsa/etf/rpr/projekat/WarehouseModel.java
@@ -4,9 +4,44 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class WarehouseModel {
     private ObservableList<Sector> sectors = FXCollections.observableArrayList();
     private SimpleObjectProperty<Product> currentProduct = new SimpleObjectProperty<>();
+    private Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    private void regenerateDB() {
+        Scanner input = null;
+        try {
+            input = new Scanner(new FileInputStream("korisnici.db.sql"));
+            String sqlQuery = "";
+            while (input.hasNext()) {
+                sqlQuery += input.nextLine();
+                if ( sqlQuery.length() > 1 && sqlQuery.charAt( sqlQuery.length()-1 ) == ';') {
+                    try {
+                        Statement stmt = connection.createStatement();
+                        stmt.execute(sqlQuery);
+                        sqlQuery = "";
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            input.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     public ObservableList<Sector> getSectors() {
         return sectors;
