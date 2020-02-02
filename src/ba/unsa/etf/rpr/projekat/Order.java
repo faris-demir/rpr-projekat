6870aa73b268d5b9612fbb7d548e3b80a7d84b34
Faris Demir <fdemir1@etf.unsa.ba>
2020-02-02 01:36:06 +0100

    Kreirani atributi i generisani getteri i setteri za Order klasu

--- src/ba/unsa/etf/rpr/projekat/Order.java
+++ src/ba/unsa/etf/rpr/projekat/Order.java
@@ -1,4 +1,51 @@
 package ba.unsa.etf.rpr.projekat;
 
+import java.time.LocalDateTime;
+
 public class Order {
+    private String productName;
+    private int orderedQuantity;
+    private LocalDateTime orderDate;
+    private int price;
+    private int totalPrice;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getOrderedQuantity() {
+        return orderedQuantity;
+    }
+
+    public void setOrderedQuantity(int orderedQuantity) {
+        this.orderedQuantity = orderedQuantity;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
