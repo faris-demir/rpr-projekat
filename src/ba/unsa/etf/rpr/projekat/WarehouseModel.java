b9095a693b0c1a096e7209946fcd461ecef48042
Faris Demir <fdemir1@etf.unsa.ba>
2020-02-03 01:37:23 +0100

    Napisana metoda loadData za punjenje forme podacima iz baze

--- src/ba/unsa/etf/rpr/projekat/WarehouseModel.java
+++ src/ba/unsa/etf/rpr/projekat/WarehouseModel.java
@@ -13,7 +13,7 @@ public class WarehouseModel {
     private ObservableList<Sector> sectors = FXCollections.observableArrayList();
     private SimpleObjectProperty<Product> currentProduct = new SimpleObjectProperty<>();
     private Connection connection;
-    private PreparedStatement getAllData;
+    private PreparedStatement getAllData, getAllSectors, getAllContainers, getAllProducts;
 
     public Connection getConnection() {
         return connection;
@@ -44,7 +44,7 @@ public class WarehouseModel {
 
     public WarehouseModel() {
         try {
-            connection = DriverManager.getConnection("jdbc:sqlite:korisnici.db");
+            connection = DriverManager.getConnection("jdbc:sqlite:warehouse.db");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -59,12 +59,59 @@ public class WarehouseModel {
                 e1.printStackTrace();
             }
         }
+
+        try {
+            getAllSectors = connection.prepareStatement("select * from sector");
+            getAllContainers = connection.prepareStatement("select c.id, c.name, c.capacity, c.sector_id from container c, sector s where c.sector_id = s.id;");
+            getAllProducts = connection.prepareStatement("select p.id, p.name, p.quantity, p.weight, p.unit, p.package_height, p.package_width, p.serial_number, " +
+                                                              " p.location_tag, p.purchase_price, p.selling_price, container_id from product p, container c, sector s " +
+                                                              "where p.container_id = c.id and c.sector_id = s.id;");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void loadData() {
         try {
-            ResultSet rs = getAllData.executeQuery();
+            ResultSet rs1 = getAllSectors.executeQuery();
+            while (rs1.next()) {
+                Sector newSector = new Sector(rs1.getInt(1), rs1.getString(2), rs1.getInt(3));
+                sectors.add(newSector);
+            }
 
+            ResultSet rs2 = getAllContainers.executeQuery();
+            while (rs2.next()) {
+                Container newContainer = new Container(rs2.getInt(1), rs2.getString(2), rs2.getDouble(3));
+                for (Sector s : sectors) {
+                    if (s.getId() == rs2.getInt(4)) {
+                        s.getContainers().add(newContainer);
+                    }
+                }
+            }
+
+            ResultSet rs3 = getAllProducts.executeQuery();
+            while (rs3.next()) {
+                Product newProduct = new Product();
+                newProduct.setId(rs3.getInt(1));
+                newProduct.setName(rs3.getString(2));
+                newProduct.setQuantity(rs3.getInt(3));
+                newProduct.setWeight(rs3.getDouble(4));
+                newProduct.setUnit(rs3.getString(5));
+                newProduct.setPackageHeight(rs3.getDouble(6));
+                newProduct.setPackageWidth(rs3.getDouble(7));
+                newProduct.setSerialNumber(rs3.getString(8));
+                newProduct.setLocationTag(rs3.getString(9));
+                newProduct.setPurchasePrice(rs3.getDouble(10));
+                newProduct.setSellingPrice(rs3.getDouble(11));
+                for (Sector s : sectors) {
+                    for (Container c : s.getContainers()) {
+                        if (c.getId() == rs3.getInt(12)) {
+                            c.getProducts().add(newProduct);
+                        }
+                    }
+                }
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
