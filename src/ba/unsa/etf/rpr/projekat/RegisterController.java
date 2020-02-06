44bfcecfc75890a6f09eb9f75e13812cbb5f14dc
Faris Demir <fdemir1@etf.unsa.ba>
2020-02-07 00:47:47 +0100

    Omogućena validacija polja na register formi

--- src/ba/unsa/etf/rpr/projekat/RegisterController.java
+++ src/ba/unsa/etf/rpr/projekat/RegisterController.java
@@ -38,7 +38,85 @@ public class RegisterController {
         cbUnit.setItems(unitObsList);
         cbUnit.setValue("tonne");
 
+        fldName.textProperty().addListener(((obs, oldVal, newVal) -> {
+            if (!newVal.isEmpty()) {
+                fldName.getStyleClass().removeAll("notValidField");
+                fldName.getStyleClass().add("validField");
+            } else {
+                fldName.getStyleClass().removeAll("validField");
+                fldName.getStyleClass().add("notValidField");
+            }
+        }));
 
+        fldQuantity.textProperty().addListener(((obs, oldVal, newVal) -> {
+            if (!newVal.isEmpty()) {
+                fldQuantity.getStyleClass().removeAll("notValidField");
+                fldQuantity.getStyleClass().add("validField");
+            } else {
+                fldQuantity.getStyleClass().removeAll("validField");
+                fldQuantity.getStyleClass().add("notValidField");
+            }
+        }));
+
+        fldWeight.textProperty().addListener(((obs, oldVal, newVal) -> {
+            if (!newVal.isEmpty()) {
+                fldWeight.getStyleClass().removeAll("notValidField");
+                fldWeight.getStyleClass().add("validField");
+            } else {
+                fldWeight.getStyleClass().removeAll("validField");
+                fldWeight.getStyleClass().add("notValidField");
+            }
+        }));
+
+        fldHeight.textProperty().addListener(((obs, oldVal, newVal) -> {
+            if (!newVal.isEmpty()) {
+                fldHeight.getStyleClass().removeAll("notValidField");
+                fldHeight.getStyleClass().add("validField");
+            } else {
+                fldHeight.getStyleClass().removeAll("validField");
+                fldHeight.getStyleClass().add("notValidField");
+            }
+        }));
+
+        fldWidth.textProperty().addListener(((obs, oldVal, newVal) -> {
+            if (!newVal.isEmpty()) {
+                fldWidth.getStyleClass().removeAll("notValidField");
+                fldWidth.getStyleClass().add("validField");
+            } else {
+                fldWidth.getStyleClass().removeAll("validField");
+                fldWidth.getStyleClass().add("notValidField");
+            }
+        }));
+
+        fldSerial.textProperty().addListener(((obs, oldVal, newVal) -> {
+            if (!newVal.isEmpty()) {
+                fldSerial.getStyleClass().removeAll("notValidField");
+                fldSerial.getStyleClass().add("validField");
+            } else {
+                fldSerial.getStyleClass().removeAll("validField");
+                fldSerial.getStyleClass().add("notValidField");
+            }
+        }));
+
+        fldPurchasePrice.textProperty().addListener(((obs, oldVal, newVal) -> {
+            if (!newVal.isEmpty()) {
+                fldPurchasePrice.getStyleClass().removeAll("notValidField");
+                fldPurchasePrice.getStyleClass().add("validField");
+            } else {
+                fldPurchasePrice.getStyleClass().removeAll("validField");
+                fldPurchasePrice.getStyleClass().add("notValidField");
+            }
+        }));
+
+        fldSellingPrice.textProperty().addListener(((obs, oldVal, newVal) -> {
+            if (!newVal.isEmpty()) {
+                fldSellingPrice.getStyleClass().removeAll("notValidField");
+                fldSellingPrice.getStyleClass().add("validField");
+            } else {
+                fldSellingPrice.getStyleClass().removeAll("validField");
+                fldSellingPrice.getStyleClass().add("notValidField");
+            }
+        }));
     }
 
     public void cancelAction(ActionEvent actionEvent) {
