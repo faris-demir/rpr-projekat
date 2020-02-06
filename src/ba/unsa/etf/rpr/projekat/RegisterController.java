fbbc5eaf37dd0ecc0707e1fdf38544eb42f7e494
Faris Demir <fdemir1@etf.unsa.ba>
2020-02-06 23:24:13 +0100

    Kreirana kontroler klasa sa atributima za register formu

---
+++ src/ba/unsa/etf/rpr/projekat/RegisterController.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rpr.projekat;
+
+import javafx.scene.control.TextField;
+
+public class RegisterController {
+    public TextField fldName;
+    public TextField fldQuantity;
+    public TextField fldWeight;
+    public TextField fldUnit;
+    public TextField fldHeight;
+    public TextField fldWidth;
+    public TextField fldSerial;
+    public TextField spnSector;
+    public TextField spnContainer;
+    public TextField fldPurchasePrice;
+    public TextField fldSellingPrice;
+}
