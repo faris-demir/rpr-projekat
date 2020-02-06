4c81eba8b203a46954a11d6bcfe6ed2045f2f4fd
Faris Demir <fdemir1@etf.unsa.ba>
2020-02-07 00:22:12 +0100

    Kreirana initialize metoda u RegisterController

--- src/ba/unsa/etf/rpr/projekat/RegisterController.java
+++ src/ba/unsa/etf/rpr/projekat/RegisterController.java
@@ -1,7 +1,12 @@
 package ba.unsa.etf.rpr.projekat;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -9,7 +14,7 @@ public class RegisterController {
     public TextField fldName;
     public TextField fldQuantity;
     public TextField fldWeight;
-    public TextField fldUnit;
+    public ChoiceBox<String> cbUnit;
     public TextField fldHeight;
     public TextField fldWidth;
     public TextField fldSerial;
@@ -17,6 +22,22 @@ public class RegisterController {
     public Spinner<Integer> spnContainer;
     public TextField fldPurchasePrice;
     public TextField fldSellingPrice;
+
+    @FXML
+    public void initialize() {
+        SpinnerValueFactory<Integer> containerValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1,9,1);
+        spnContainer.setValueFactory(containerValueFactory);
+
+        ObservableList<String> sectorObsList = FXCollections.observableArrayList();
+        sectorObsList.addAll("A","B","C","D","E","F","G","H");
+        SpinnerValueFactory<String> sectorValueFactory = new SpinnerValueFactory.ListSpinnerValueFactory<>(sectorObsList);
+        spnSector.setValueFactory(sectorValueFactory);
+
+        ObservableList<String> unitObsList = FXCollections.observableArrayList();
+        unitObsList.addAll("tonne","kilogram","gram","liter","milliliter","gallon","barrel","ounce","pound");
+        cbUnit.setItems(unitObsList);
+        cbUnit.setValue("tonne");
+    }
 
     public void cancelAction(ActionEvent actionEvent) {
         Stage currentStage = (Stage) fldName.getScene().getWindow();
