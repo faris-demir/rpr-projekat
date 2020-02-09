51555703e426beeab66e864b6864c57b40ce12c3
Faris Demir <fdemir1@etf.unsa.ba>
2020-02-09 01:13:46 +0100

    Kreiran kontroler vezan za formu sell

--- src/ba/unsa/etf/rpr/projekat/SellController.java
+++ src/ba/unsa/etf/rpr/projekat/SellController.java
@@ -1,15 +1,18 @@
 package ba.unsa.etf.rpr.projekat;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class SellController {
-    public Label lbName;
-    public Label lbAvailQuantity;
-    public Label lbPrice;
+    public TextField fldName;
+    public TextField fldAvailQuantity;
+    public TextField fldPrice;
     public Spinner<Integer> spnSellQuantity;
-    private Product productForSale = new Product();
+    private Product productForSale;
 
     public SellController(Product productForSale) {
         this.productForSale = productForSale;
@@ -17,11 +20,19 @@ public class SellController {
 
     @FXML
     public void initialize() {
-        lbName.setText(productForSale.getName());
-        lbAvailQuantity.setText(String.valueOf(productForSale.getQuantity()));
-        lbPrice.setText(String.valueOf(productForSale.getSellingPrice()));
-
+        fldName.setText(productForSale.getName());
+        fldAvailQuantity.setText(String.valueOf(productForSale.getQuantity()));
+        fldPrice.setText(String.valueOf(productForSale.getSellingPrice()));
+        SpinnerValueFactory<Integer> quantityValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, productForSale.getQuantity(),1);
+        spnSellQuantity.setValueFactory(quantityValueFactory);
+        fldName.setDisable(true);
+        fldAvailQuantity.setDisable(true);
+        fldPrice.setDisable(true);
     }
 
+    public void cancelAction(ActionEvent actionEvent) {
+        Stage currentStage = (Stage) fldName.getScene().getWindow();
+        currentStage.close();
+    }
 
 }
